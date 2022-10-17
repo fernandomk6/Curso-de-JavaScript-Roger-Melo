6f07894ae7dd4a8147e8ed27f9b3c23a47f936bc
fernando <fernandomk6@gmail.com>
2022-10-17 13:42:32 -0300

    Conclído exercício 25

---
+++ etapa-10-datas/exercicio-25/app.js
@@ -0,0 +1,101 @@
+/*
+  01
+
+  - Exiba no console o index da 1ª (e única) ocorrência do mês "Fevereiro" do 
+    array "months".
+*/
+
+const months = [
+  'Janeiro',
+  'Fevereiro',
+  'Março',
+  'Abril',
+  'Maio',
+  'Junho',
+  'Julho',
+  'Agosto',
+  'Setembro',
+  'Outubro',
+  'Novembro',
+  'Dezembro'
+]
+
+const indexOfFevereiro = months.indexOf('Fevereiro')
+// console.log(indexOfFevereiro)
+
+/*
+  02
+
+  - Crie um objeto de data que represente o momento presente;
+  - Exiba o objeto no console.
+*/
+
+const present = new Date()
+console.log('present', present)
+
+/*
+  03
+
+  - Baseado no objeto que você acabou de criar, exiba o ano atual no console.
+*/
+
+const year = present.getFullYear()
+// console.log(year)
+
+/*
+  04
+
+  - Crie um objeto de data que represente um momento passado;
+  - Exiba o objeto no console.
+*/
+
+const myBrithday = new Date(1998, 7, 21)
+console.log('myBirthday', myBrithday)
+
+/*
+  05
+
+  - Exiba, no console, a hora do objeto que você acabou de criar.
+*/
+
+const myBrithdayHour = myBrithday.getHours()
+// console.log(myBrithdayHour)
+
+/*
+  06
+
+  - Crie um objeto de data que represente um momento futuro;
+  - Exiba o objeto no console.
+*/
+
+const oneDayMilliseconds = 86400000
+const tomorrow = new Date(present.getTime() + oneDayMilliseconds)
+console.log('tomorrow', tomorrow)
+
+/*
+  07
+
+  - Exiba no console a quantidade de dias entre o momento futuro e o passado.
+*/
+
+const difference = tomorrow.getTime() - present.getTime()
+const differenceDays = difference / 1000 / 60 / 60 / 24
+
+console.log(differenceDays)
+
+/*
+  08
+  
+  Assim como a 1ª aplicação que implementamos (Quiz), considere fazer 
+  da aplicação To-do List uma peça do seu portfólio.
+
+  Desenvolva a sua versão do To-do List e, se você sentir que a desenvolveu 
+  por conta própria, considere inserí-la como parte de seu portfólio.
+
+  Caso contrário, reveja as aulas e treine novamente até que você consiga 
+  desenvolver a aplicação sozinho(a).
+
+  Você pode hospedá-la no Netlify seguindo o mesmo tutorial que recomendei na 
+  aplicação do Quiz: 
+  https://www.youtube.com/playlist?list=PLlAbYrWSYTiMGMxQf9JSoZUU1rgVpGPth
+*/
