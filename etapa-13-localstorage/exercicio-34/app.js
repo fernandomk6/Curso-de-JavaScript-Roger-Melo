f48b4b5910727555b2de9fb184a4d57ac9909c3c
fernando <fernandomk6@gmail.com>
2022-11-03 12:02:08 -0300

    Concluído exercício 34

---
+++ etapa-13-localstorage/exercicio-34/app.js
@@ -0,0 +1,59 @@
+/*
+  01
+
+  - Implemente uma função que recebe uma string por parâmetro e retorna a 
+    string invertida;
+    
+    Exemplos: 
+      - Se '123' é recebido por parâmetro, '321' deve ser retornado;
+      - Se 'abc' é recebido por parâmetro, 'cba' deve ser retornado;
+    
+  - Após implementar a função, implemente outra versão da função. Essa 2ª 
+    versão deve fazer o mesmo que a função anterior faz, mas de forma diferente.
+*/
+
+const invert = aString => aString
+  .split('')
+  .reverse()
+  .join('')
+
+
+
+const invert2 = aString => {
+  let reversedString = ''
+  for (let index = aString.length -1; index >= 0; index-- ) {
+    reversedString += aString[index]
+  }
+
+  return reversedString
+}
+
+/*
+  02
+  
+  - Descubra o que o código abaixo está fazendo e refatore-o.
+*/
+
+const numbers = [5, 20, 7, 32, 47, 15, 83, 91, 27, 33]
+const foundNumber = numbers.some(number => number === 15)
+
+// console.log(foundNumber)
+
+// let foundNumber = false
+
+// numbers.forEach(number => {
+//   if (number === 15) {
+//     foundNumber = true
+//   }
+// })
+
+// console.log(foundNumber)
+
+/*
+  03
+
+  - Refatore o código da Weather Application implementado na última aula;
+  - Dicas do que pode ser refatorado:
+    - Substituir o if/else por um código mais elegante =D
+    - Isolar a manipulação do DOM em pequenas funções de responsabilidade única.
+*/
