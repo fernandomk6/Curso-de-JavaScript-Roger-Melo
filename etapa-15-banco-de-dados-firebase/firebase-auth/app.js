9d14a508ffddcd80fba8f31d0195bf9df8510826
fernando <fernandomk6@gmail.com>
2022-12-02 12:19:59 -0300

    Concluído tela inicial básica

--- etapa-15-banco-de-dados-firebase/firebase-auth/app.js
+++ etapa-15-banco-de-dados-firebase/firebase-auth/app.js
@@ -1,1 +1,14 @@
-console.log('firebase auth')
+const loginBtn = document.querySelector('#login-btn')
+
+const login = () => {
+  console.log('clicou em logar')
+}
+
+const init = () => {
+  M.AutoInit()
+}
+
+
+loginBtn.addEventListener('click', login)
+
+init()
